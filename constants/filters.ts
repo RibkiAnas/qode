017ecc916e38c98de305d8ea64f7a57445b330e4
RibkiAnas <anasribki147@gmail.com>
2023-12-08 13:21:03 +0100

    fix: some bugs

--- constants/filters.ts
+++ constants/filters.ts
@@ -28,7 +28,6 @@ export const TagFilters = [
 
 export const HomePageFilters = [
 	{ name: "Newest", value: "newest" },
-	{ name: "Recommended", value: "recommended" },
 	{ name: "Frequent", value: "frequent" },
 	{ name: "Unanswered", value: "unanswered" },
 ];
